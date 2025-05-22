936e6bb2378c32ebb8ace8dc66dd1603baee09cb
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-22 21:07:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+	const characters="abcdefghijklmnopqrstuvwxyz0123456789"
+	let res=''
+
+	for(let i=0i<1;i++){
+		const randomText=Math.floor(Math.random()*characters.length)
+		res=res+characters[randomText]
+	}
+	return res
 }
 
 // Do not change the code below.
