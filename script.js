1e172968a80df85b4efbe9eca58892a1d8b9bc19
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-22 21:20:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function makeid(l) {
   // write your code here
 	const characters="abcdefghijklmnopqrstuvwxyz0123456789"
 	let res=''
-
+const length = Number(l);
 	for(let i=0i<1;i++){
 		const randomText=Math.floor(Math.random()*characters.length)
 		res=res+characters[randomText]
